5dec92369259dbc56cea351a27277b1140b738c4
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-01-04 11:47:05 -0700

    added to inventory

--- mud/src/generics/gameObject.js
+++ mud/src/generics/gameObject.js
@@ -4,6 +4,12 @@ class GameObject {
     this.description = options.description;
     this.inventory = [];
   }
+  addToInventory(item) {
+    this.inventory.push(item);
+  }
+  removeFromInventory(name) {
+    this.inventory = this.inventory.filter(item => item.name !== name);
+  }
 }
 
 export default gameObject;
