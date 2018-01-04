8a39ba7c09475616e7e6eb3a2defe6eb059a7479
fairlight-excalibur <lidgrt@gmail.com>
2018-01-04 13:00:12 -0600

    added basics

---
+++ mud/src/generics/person.js
@@ -0,0 +1,25 @@
+﻿import gameObject from './gameObject.js';
+
+class Person extends gameObject{
+	constructor(options){
+		super(options);
+		this.netWorth = options.netWorth;
+		this.location = options.startLocation;
+	}
+}
+
+class NPC extends Person{
+	constructor(options){
+		super(options);
+		this.barks = options.barks;
+	}
+}
+
+class Player extends  Person{
+	constructor(options){
+		super(options);
+		this.
+	}
+}
+
+export default NPC, Person;
