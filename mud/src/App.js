34df051149f61882b436db208442075277156b9e
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-01-04 13:45:29 -0700

    pulling

--- mud/src/App.js
+++ mud/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      room: undefined,
+    };
+  }
   render() {
+    let input;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* display current Room */}
+        <form onSubmit={(e) => {
+          e.preventDefault();
+        }}>
+          <input type="text" ref={node => input = node} />
+        </form>
       </div>
     );
   }
