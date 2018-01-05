c83bbae0c9f757cbf3e8bcbf611f4d4f4605b48a
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-01-05 12:27:15 -0700

    fixed minor spacing

--- mud/src/components/descriptions.js
+++ mud/src/components/descriptions.js
@@ -4,7 +4,7 @@ export default (props) => (
   <div className="Container">
     <h2>{props.name}</h2>
     <p>{props.description}</p>
-    <p>You are at the {props.name}.{Object.keys(props.directions).map((value) => {
+    <p>You are at the {props.name}. {Object.keys(props.directions).map((value) => {
       return <Fragment>
         The {props.directions[value].name} is to the
         <strong onClick={() => {props.handleInput(value)}}> {value}</strong>.
