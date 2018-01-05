a6dba49faac50c4f0e06ad09fa085cbbd8798b53
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-01-05 01:54:09 -0700

    updated item to have toggleprop instead of setprop.

--- mud/src/generics/item.js
+++ mud/src/generics/item.js
@@ -9,8 +9,9 @@ class Item extends GameObject {
     this.pickUp = this.pickUp.bind(this);
     this.addToActions('get', this.pickUp);
   }
-  setProp(property, value) {
-    this[property] = value;
+  toggleProp(property) {
+    this[property] = !this[property];
+    return this[property];
   }
   getProp(property) {
     return this[property];
