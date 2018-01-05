beb21bb81a0b7a779d6619cbb04066947713839f
etisdew <etisdew@github.com>
2018-01-04 22:10:09 -0800

    italic items

--- mud/src/components/descriptions.js
+++ mud/src/components/descriptions.js
@@ -3,15 +3,16 @@ import React, { Fragment } from 'react';
 export default (props) => (
   <div className="Container">
     <h2>{props.name}</h2>
+    <p>{props.description}</p>
     <p>You are at the {props.name}.{Object.keys(props.directions).map((value) => {
       return <Fragment>
         The {props.directions[value].name} is to the
-        <span className="Direction" onClick={() => {props.handleInput(value)}}> {value}</span>.
+        <strong onClick={() => {props.handleInput(value)}}> {value}</strong>.
       </Fragment>
     })}</p>
     <p>{props.inventory.map((item) => {
       if (item.name !== props.playerName)
-        return <Fragment>There is a {item.name}.</Fragment>;
+        return <Fragment>There is a <em className="Item">{item.name}</em>.</Fragment>;
     })}</p>
   </div>
 );
