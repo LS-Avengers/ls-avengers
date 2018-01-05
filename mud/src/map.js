405036c571358d641057695405a0b709f4f27201
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-01-04 22:00:16 -0700

    removed commented code

--- mud/src/map.js
+++ mud/src/map.js
@@ -1,31 +1,6 @@
 import Room from './generics/room.js';
 import snapple from './specifics/snappleBottle.js';
 
-// var r1 = new Room({
-//   name: 'Starting Room',
-//   description: 'this is the place you start.',
-//   // directions: {north: r2},
-// });
-// var r2 = new Room({
-//   name: 'Hallway',
-//   description: 'a generic hallway.',
-//   // directions: {
-//   //   south: r1,
-//   //   north: r3,
-//   // },
-// });
-// var r3 = new Room({
-//   name: 'The End',
-//   description: 'the end of the test area.',
-//   // directions: {south: r2}
-// });
-// r1.addNewDirection('north', r2);
-// r2.addNewDirection('south', r1);
-// r2.addNewDirection('north', r3);
-// r3.addNewDirection('south', r2);
-//
-// export default [r1, r2, r3];
-
 // Introduction
 // Apartment Rooms
 var bedroom = new Room({
